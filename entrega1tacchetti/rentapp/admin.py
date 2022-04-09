ae3de729f8f3619b67d7b775bdf752976626cce1
FrancoTacchetti <taccho87@gmail.com>
2022-04-09 12:04:26 -0300

    Add Html, Css and Bootstrap

---
+++ entrega1tacchetti/rentapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from.models import Tenant, RentPlace, RentCar
+
+# Register your models here.
+admin.site.register(Tenant)
+admin.site.register(RentPlace)
+admin.site.register(RentCar)
--- entrega1tacchetti/rentapp/views.py
+++ entrega1tacchetti/rentapp/views.py
@@ -1,3 +1,50 @@
-from multiprocessing import context
-from django.views.generic import TemplateView
 from django.shortcuts import render
+from multiprocessing import context
+from django.views.generic import (
+    TemplateView,
+    ListView, 
+    CreateView)
+from .models import (
+    Tenant, 
+    RentCar, 
+    RentPlace)
+
+
+class HomeView(TemplateView):
+
+    # Redirect To Other Views
+    pass
+
+class TenantCreateView(CreateView):
+    model = Tenant
+    fields = [
+            "first_name",
+            "last_name",
+            "email",
+            "paid_method",
+            "rent_from",
+            "rent_to",
+        ]
+
+class RentPlaceCreateView(CreateView):
+    model = RentPlace
+    fields = [
+            "location",
+            "rooms",
+            "bathrooms",
+            "garage",
+            "price",
+            "capacity",
+    ]
+
+class RentCarCreateView(CreateView):
+    model = RentCar
+    fields = [
+            "car_model",
+            "price",
+            "model_year",
+            "car_insurance",
+        ]
+
+
+
