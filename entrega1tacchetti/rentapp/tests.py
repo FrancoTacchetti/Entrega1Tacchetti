ee763c446247aeddf7bb329ee719e46aa2ed2935
FrancoTacchetti <taccho87@gmail.com>
2022-05-02 20:50:57 -0300

    Add unittests

--- entrega1tacchetti/rentapp/tests.py
+++ entrega1tacchetti/rentapp/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
 
 # Create your tests here.
+class CreateRouteTestCases(SimpleTestCase):
+
+    def test_create_tenant_route(self):
+        url = reverse('tenant-create')
+        self.assertEquals(resolve(url).route, 'tenant/new')
+    
+    def test_create_rentable_place_route(self):
+        url = reverse('rentable-place-create')
+        self.assertEquals(resolve(url).route, 'place/new')
+
+    def test_create_rentable_car_route(self):
+        url = reverse('rentable-car-create')
+        self.assertEquals(resolve(url).route, 'car/new')
